3f6fd0344f022da3a420b741e5ae6f597ca21473
Gregor Tätzner <mail@gtaetzner.de>
2019-02-26 17:44:35 +0100

    Fix #1 Add sample test configuration

---
+++ config/test.sample.exs
@@ -0,0 +1,8 @@
+use Mix.Config
+
+# api key for text analytics
+config :cognixir, ta_api_key: "your key"
+# api key for computer vision
+config :cognixir, cv_api_key: "your key"
+# api key for face api
+config :cognixir, fa_api_key: "your key"
